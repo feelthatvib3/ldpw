fc977f7e006a0590e98c66b63d1ed8923a129017
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2022-12-28 22:26:41 +0100

    configure tailwindcss theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light_primary: "#fafafa",
+        dark_primary: "#111111",
+
+        light_header_text: "#666666",
+        light_header_text_hover: "#000000",
+
+        dark_header_text: "#888888",
+        dark_header_text_hover: "#ffffff",
+
+        light_border: "#eaeaea",
+        dark_border: "#333333",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+    },
   },
   plugins: [],
 };
