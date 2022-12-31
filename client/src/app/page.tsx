ca5935c5a2e7e7984bed18cf636ac53d256ff716
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2022-12-31 20:14:18 +0100

    add component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import { Hero } from "../layout/Hero";
+
 export default function Home() {
   return (
-    <main>
-      <div></div>
-    </main>
+    <div className="pt-20">
+      <Hero />
+    </div>
   );
 }
--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,11 +1,40 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+
+import logoDark from "../../../client/public/logo-dark.png";
+import logoLight from "../../../client/public/logo-light.png";
+
+import Logo from "../components/logo/Logo";
+import MenuButton from "../components/menu-button/MenuButton";
+import { NavigationMobile } from "../components/navigation-mobile/NavigationMobile";
+import { Navigation } from "../components/navigation/Navigation";
 
 export const Header = () => {
+  const [mobileNav, setMobileNav] = useState(false);
   return (
-    <div>
-      <div>Header</div>
-    </div>
+    <header className="fixed w-full border-b border-b-light_border bg-white dark:border-b-dark_border dark:bg-black">
+      <nav className="container flex max-w-screen-lg items-center justify-between py-2">
+        <div>
+          <Logo logoLight={logoLight} logoDark={logoDark} href={"/"} />
+        </div>
+        {/* mobile navigation button | shown by default | hidden on desktop mode */}
+        <div className="lg:hidden" onClick={() => setMobileNav(!mobileNav)}>
+          <MenuButton />
+        </div>
+        {/* navigation | hidden by default | shown on desktop mode */}
+        <div className="hidden lg:block">
+          <Navigation />
+        </div>
+      </nav>
+      {/* mobile navigation | available by default | unavailable on desktop mode */}
+      <div
+        className={`${
+          mobileNav ? "max-h-full" : "max-h-0 overflow-hidden"
+        } fixed left-0 h-full w-full bg-white transition-all dark:bg-black`}
+      >
+        <NavigationMobile />
+      </div>
+    </header>
   );
 };
