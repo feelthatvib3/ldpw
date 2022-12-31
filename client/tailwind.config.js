2dd5fd31cb24664a2c980bb58475ac121b7b364c
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2022-12-31 20:13:19 +0100

    update tailwindcss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layout/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
